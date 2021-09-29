9b509e99b7d457e4e7ec619d5ba6f985fd674b43
Enos Andrade <enosads@gmail.com>
2021-09-29 17:17:39 -0300

    added categories routes

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,30 @@
+import { Category } from "../models/Category";
+
+interface ICreateCategoryDTO {
+  name: string;
+  description: string;
+}
+
+class CategoriesRepository {
+  private readonly categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICreateCategoryDTO): void {
+    const category = new Category();
+    Object.assign(category, { name, description, created_at: new Date() });
+    this.categories.push(category);
+  }
+
+  list(): Category[] {
+    return this.categories;
+  }
+
+  findByName(name: string): Category {
+    return this.categories.find((category) => category.name === name);
+  }
+}
+
+export { CategoriesRepository };
